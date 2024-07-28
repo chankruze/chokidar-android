5d8f1d2657640ab8f03b53776b48fcbe18faf5b9
chankruze <chankruze@gmail.com>
2024-07-28 11:24:51 +0530

    fix(service): complete AntiTouchAlertService service
    Signed-off-by: chankruze <chankruze@gmail.com>

--- app/src/main/java/com/geekofia/phonepolice/services/AntiTouchAlertService.java
+++ app/src/main/java/com/geekofia/phonepolice/services/AntiTouchAlertService.java
@@ -1,10 +1,17 @@
 package com.geekofia.phonepolice.services;
 
 import static android.content.pm.ServiceInfo.FOREGROUND_SERVICE_TYPE_MEDIA_PLAYBACK;
+import static com.geekofia.phonepolice.utils.Constants.ANTI_TOUCH_ALERT_NOTIFICATION_ID;
 import static com.geekofia.phonepolice.utils.Constants.ANTI_TOUCH_ALERT_SERVICE_ID;
 import static com.geekofia.phonepolice.utils.Constants.ANTI_TOUCH_ALERT_SERVICE_NOTIFICATION_CHANNEL_ID;
 import static com.geekofia.phonepolice.utils.Constants.ANTI_TOUCH_ALERT_SERVICE_TAG;
+import static com.geekofia.phonepolice.utils.Constants.FULL_BATTERY_ALERT_NOTIFICATION_ID;
+import static com.geekofia.phonepolice.utils.Constants.FULL_BATTERY_ALERT_SERVICE_NOTIFICATION_CHANNEL_ID;
 import static com.geekofia.phonepolice.utils.Utils.createNotificationChannel;
+import static com.geekofia.phonepolice.utils.Utils.dismissNotification;
+import static com.geekofia.phonepolice.utils.Utils.getSelectedTone;
+import static com.geekofia.phonepolice.utils.Utils.setupMediaPlayer;
+import static com.geekofia.phonepolice.utils.Utils.showNotification;
 
 import android.app.Notification;
 import android.app.NotificationManager;
@@ -16,8 +23,10 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.IBinder;
+import android.os.Vibrator;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
@@ -28,27 +37,20 @@ import com.geekofia.phonepolice.R;
 import com.geekofia.phonepolice.activities.AntiTouchAlertActivity;
 import com.geekofia.phonepolice.utils.Constants;
 import com.geekofia.phonepolice.utils.PreferenceKeyManager;
+import com.geekofia.phonepolice.utils.Utils;
 
 public class AntiTouchAlertService extends Service implements SensorEventListener {
     private float acceleration;
     private float accelerationCurrent;
     private float accelerationLast;
     private SensorManager sensorManager;
+    private MediaPlayer mediaPlayer;
+    private String currentAlertTone;
 
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
         return null;
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
     }
 
     @Override
@@ -68,18 +70,17 @@ public class AntiTouchAlertService extends Service implements SensorEventListene
 
         if (isAntiTouchAlertEnabled) {
             // TODO: register sensor listener
+            sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+            Sensor defaultSensor = sensorManager.getDefaultSensor(1);
+            acceleration = 0.0f;
+            accelerationCurrent = 9.80665f;
+            accelerationLast = 9.80665f;
+            sensorManager.registerListener(this, defaultSensor, 0);
         }
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        Sensor defaultSensor = sensorManager.getDefaultSensor(1);
-        acceleration = 0.0f;
-        accelerationCurrent = 9.80665f;
-        accelerationLast = 9.80665f;
-        sensorManager.registerListener(this, defaultSensor, 0);
-
         Log.d(ANTI_TOUCH_ALERT_SERVICE_TAG, "onStartCommand executed");
 
         // Create a pending intent to open when clicked on the notification
@@ -108,12 +109,80 @@ public class AntiTouchAlertService extends Service implements SensorEventListene
     @Override
     public void onDestroy() {
         super.onDestroy();
-        // TODO: pause media player
+        // Cleanup the media player
+        if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
         // TODO: disable flash
         if (sensorManager != null) {
             sensorManager.unregisterListener(this);
         }
         // Dismiss the service with notification
         stopForeground(true);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if (sensorEvent.sensor.getType() == 1) {
+            float[] fArr = (float[]) sensorEvent.values.clone();
+            float f1 = fArr[0];
+            float f2 = fArr[1];
+            float f3 = fArr[2];
+
+            accelerationLast = accelerationCurrent;
+
+            float sqrt = (float) Math.sqrt(Math.pow(f1, 2) + Math.pow(f2, 2) + Math.pow(f3, 2));
+            accelerationCurrent = sqrt;
+
+            float f4 = (acceleration * 0.5f) + (sqrt - accelerationLast);
+            acceleration = f4;
+
+            if (f4 > 1.0f) {
+                // TODO: play alert
+                // TODO: set flash
+                // TODO: set vibrate
+                // Get selected tone
+                String selectedTone = getSelectedTone(this, PreferenceKeyManager.getPreferenceKeyItem(Constants.ANTI_TOUCH_ALERT_TONE_PICKER).getKey());
+
+                // Check if currently selected tone is changed
+                if (!selectedTone.equals(currentAlertTone) || mediaPlayer == null) {
+                    // Release current media player if tone has changed
+                    if (mediaPlayer != null) {
+                        mediaPlayer.release();
+                    }
+                    currentAlertTone = selectedTone;
+                    mediaPlayer = setupMediaPlayer(this, currentAlertTone, false);
+                }
+                // If media player is already playing, start playing
+                if (!mediaPlayer.isPlaying()) {
+                    mediaPlayer.start();
+                    // Create a new notification and swap it with existing one
+                    Notification notification = new NotificationCompat.Builder(this, ANTI_TOUCH_ALERT_SERVICE_NOTIFICATION_CHANNEL_ID)
+                            .setContentTitle("Anti Touch Alert")
+                            .setContentText("Your device is being moved.")
+                            .setSmallIcon(R.drawable.ic_anti_touch)
+                            .setOngoing(true)
+                            .build();
+                    // Show the notification
+                    showNotification(this, ANTI_TOUCH_ALERT_NOTIFICATION_ID, notification);
+                }
+            }
+            // TODO: properly exit by removing notification once not moving
+            //            else {
+            //                if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+            //                    // Pause the media player
+            //                    mediaPlayer.pause();
+            //                    // Dismiss the notification
+            //                    dismissNotification(this, ANTI_TOUCH_ALERT_NOTIFICATION_ID);
+            //                }
+            //            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
