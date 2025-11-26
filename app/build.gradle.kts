72d08ccca1f865cd7c9ef40b361df5bfd19a33b5
Chandan Kumar Mandal <chankruze@gmail.com>
2025-11-26 11:02:22 +0530

    Configure signing for release build type
    Added signing configuration for release build type and updated release build settings.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,13 +16,25 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("my-release-key.jks")
+            storePassword = System.getenv("ANDROID_KEYSTORE_PASSWORD")
+            keyAlias = System.getenv("ANDROID_KEY_ALIAS")
+            keyPassword = System.getenv("ANDROID_KEY_PASSWORD")
+        }
+    }
+
     buildTypes {
-        release {
-            isMinifyEnabled = true
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = false
+            isShrinkResources = false
+        }
+
+        getByName("debug") {
+            // optional
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 
